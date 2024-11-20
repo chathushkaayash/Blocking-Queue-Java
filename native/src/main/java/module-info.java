508116e226f51578ac9406b8d666db2ecc8e16ba
ayash <chathushkaayash@gmail.com>
2024-11-20 14:52:32 +0530

    remove time-native lib

--- native/src/main/java/module-info.java
+++ native/src/main/java/module-info.java
@@ -1,8 +1,6 @@
-
 module io.ayash.blockingqueue {
     requires io.ballerina.runtime;
     requires io.ballerina.lang.value;
-    requires io.ballerina.stdlib.time;
 
     exports io.ayash.blockingqueue;
 }
