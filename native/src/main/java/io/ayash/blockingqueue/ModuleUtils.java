a056fa433a60d43b94707b31997ffbe3b55edffa
M.C.A. Maddumage <102851155+chathushkaayash@users.noreply.github.com>
2024-12-09 18:29:33 +0530

    Update ModuleUtils.java

--- native/src/main/java/io/ayash/blockingqueue/ModuleUtils.java
+++ native/src/main/java/io/ayash/blockingqueue/ModuleUtils.java
@@ -3,10 +3,6 @@ package io.ayash.blockingqueue;
 import io.ballerina.runtime.api.Environment;
 import io.ballerina.runtime.api.Module;
 
-/**
- * Module utils for the Ballerina AWS Marketplace Entitlement connector to
- * obtain the module info in the init.
- */
 public final class ModuleUtils {
 
     private static Module module;
